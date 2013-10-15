5106176d22777de9ca1fd3e780ee7b32d5d1dcc4
shelltea <shelltea@gmail.com>
2013-10-15 16:18:46 +0800

    增加jquery.autocomplete插件

--- src/main/webapp/WEB-INF/resources/scripts/sea-modules/ikj/kjquery/1.0.0/kjquery-debug.js
+++ src/main/webapp/WEB-INF/resources/scripts/sea-modules/ikj/kjquery/1.0.0/kjquery-debug.js
@@ -1,4 +1,4 @@
-define("ikj/kjquery/1.0.0/kjquery-debug", [ "$-debug", "ikj/jquery-ui/1.10.3/jquery-ui-debug", "ikj/rest/1.0.0/rest-debug", "ikj/ztree/3.5.14/ztree-debug", "ikj/gritter/1.0.0/gritter-debug", "ikj/bootstrap/2.3.2/bootstrap-debug", "ikj/datepicker/1.2.0/datepicker-debug", "ikj/daterangepicker/1.2.0/daterangepicker-debug", "ikj/serialize-object/1.0.0/serialize-object-debug" ], function(require, exports, module) {
+define("ikj/kjquery/1.0.0/kjquery-debug", [ "$-debug", "ikj/jquery-ui/1.10.3/jquery-ui-debug", "ikj/rest/1.0.0/rest-debug", "ikj/ztree/3.5.14/ztree-debug", "ikj/gritter/1.0.0/gritter-debug", "ikj/bootstrap/2.3.2/bootstrap-debug", "ikj/datepicker/1.2.0/datepicker-debug", "ikj/daterangepicker/1.2.0/daterangepicker-debug", "ikj/serialize-object/1.0.0/serialize-object-debug", "ikj/autocomplete/1.1.0/autocomplete-debug" ], function(require, exports, module) {
     var $ = require("$-debug");
     require("ikj/jquery-ui/1.10.3/jquery-ui-debug");
     require("ikj/rest/1.0.0/rest-debug");
@@ -8,5 +8,6 @@ define("ikj/kjquery/1.0.0/kjquery-debug", [ "$-debug", "ikj/jquery-ui/1.10.3/jqu
     require("ikj/datepicker/1.2.0/datepicker-debug");
     require("ikj/daterangepicker/1.2.0/daterangepicker-debug");
     require("ikj/serialize-object/1.0.0/serialize-object-debug");
+    require("ikj/autocomplete/1.1.0/autocomplete-debug");
     module.exports = $;
 });
--- src/main/webapp/WEB-INF/views/common/head.jsp
+++ src/main/webapp/WEB-INF/views/common/head.jsp
@@ -15,6 +15,7 @@
 <link href="${resources}/styles/ui/ace-responsive.min.css" rel="stylesheet">
 <link href="${resources}/styles/ui/ace-skins.min.css" rel="stylesheet">
 <link href="${resources}/styles/ui/zTreeStyle/zTreeStyle.css" rel="stylesheet">
+<link href="${resources}/styles/ui/jquery.autocomplete.css" rel="stylesheet">
 <link href="${resources}/styles/main.css" rel="stylesheet">
 <!--[if IE 7]>
 <link href="${resources}/styles/ui/font-awesome-ie7.min.css" rel="stylesheet"/>
