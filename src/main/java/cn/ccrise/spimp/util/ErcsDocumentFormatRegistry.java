2031843d5ea1613bd7af5238e51e60be10f7aa13
zhaopeifu <344293750@qq.com>
2013-10-16 11:02:24 +0800

    扩展openoffice 操作库 对高版本的office的支持

---
+++ src/main/java/cn/ccrise/spimp/util/ErcsDocumentFormatRegistry.java
@@ -0,0 +1,34 @@
+package cn.ccrise.spimp.util;
+import com.artofsolving.jodconverter.DefaultDocumentFormatRegistry;
+import com.artofsolving.jodconverter.DocumentFamily;
+import com.artofsolving.jodconverter.DocumentFormat;
+
+public class ErcsDocumentFormatRegistry extends DefaultDocumentFormatRegistry {
+
+	public ErcsDocumentFormatRegistry() {
+		super();
+		/**
+		 * 解决docx 不能转化的问题
+		 */
+		final DocumentFormat docx = new DocumentFormat("Microsoft Word", DocumentFamily.TEXT, "application/msword",
+				"docx");
+		docx.setExportFilter(DocumentFamily.TEXT, "MS Word 97");
+		addDocumentFormat(docx);
+		/**
+		 * 解决xlsx 不能转化的问题
+		 */
+		final DocumentFormat xlsx = new DocumentFormat("Microsoft Excel", DocumentFamily.SPREADSHEET,
+				"application/vnd.ms-excel", "xlsx");
+		xlsx.setExportFilter(DocumentFamily.SPREADSHEET, "MS Excel 97");
+		addDocumentFormat(xlsx);
+	}
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
