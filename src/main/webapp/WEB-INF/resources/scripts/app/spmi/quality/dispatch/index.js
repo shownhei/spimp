7db537745783e4e4a970087414f0a59095bc0f8b
shelltea <shelltea@gmail.com>
2013-11-04 14:53:34 +0800

    增加调度专业评分验证 #128 优化评分表显示效果

--- src/main/webapp/WEB-INF/resources/scripts/app/spmi/quality/dispatch/index.js
+++ src/main/webapp/WEB-INF/resources/scripts/app/spmi/quality/dispatch/index.js
@@ -22,38 +22,22 @@ define(function(require, exports, module) {
 	}
 	$('#monthSelect').html(monthOptionsHtml);
 
-	// 评分表文本框
-	$.each($('#grade-record-table tr:not(:first)'), function(key, value) {
-		var tds = $(value).find('td');
-		$.each(tds, function(k, v) {
-			if (tds.length >= 5) {
-				if (k === tds.length - 2) {
-					$(v).html(
-							'<textarea name="grade-record-' + (key + 1) + '-' + (k + (8 - tds.length + 1))
-									+ '" style="width: 146px; height: 100%;resize: none;border: 0"></textarea>');
-				}
-				if (k === tds.length - 1) {
-					$(v).html(
-							'<input name="grade-record-' + (key + 1) + '-' + (k + (8 - tds.length + 1))
-									+ '" type="text" style="width: 26px; height: 100%;border: 0" value="' + $(v).prev().prev().html() + '">');
-				}
-			}
-		});
-	});
-
 	// 配置表格列
 	var fields = [ {
 		header : '年份',
-		name : 'year'
+		name : 'year',
+		align : 'right'
 	}, {
 		header : '月份',
-		name : 'month'
+		name : 'month',
+		align : 'right'
 	}, {
 		header : '专业',
 		name : 'category'
 	}, {
 		header : '分数',
-		name : 'score'
+		name : 'score',
+		align : 'right'
 	}, {
 		header : '评分时间',
 		name : 'gradedTime',
@@ -80,7 +64,7 @@ define(function(require, exports, module) {
 	}
 
 	// 配置表格
-	var defaultUrl = contextPath + '/spmi/quality/grades?orderBy=id&order=desc&pageSize=' + pageSize;
+	var defaultUrl = contextPath + '/spmi/quality/grades?orderBy=year,month&order=desc,desc&pageSize=' + pageSize;
 	var grid = new Grid({
 		parentNode : '#grade-table',
 		url : defaultUrl,
@@ -98,9 +82,33 @@ define(function(require, exports, module) {
 		}
 	}).render();
 
+	// 评分表文本框
+	$.each($('#grade-record-table tr:not(:first)'), function(key, value) {
+		var tds = $(value).find('td');
+		$.each(tds, function(k, v) {
+			if (tds.length >= 5) {
+				var rank = (key + 1) + '-' + (k + (8 - tds.length + 1));
+				var standardScore = $(v).prev().prev().html();
+
+				if (k === tds.length - 2) {
+					$(v).html('<textarea name="grade-record-' + rank + '" style="width: 146px; height: 100%;resize: none;border: 0"></textarea>');
+				}
+				if (k === tds.length - 1) {
+					$(v).html(
+							'<input name="grade-record-' + rank + '" type="text" style="width: 26px; height: 100%;border: 0" data-max="' + standardScore
+									+ '" value="' + standardScore + '">');
+				}
+			}
+		});
+	});
+
 	// 新建
 	$('#create').click(function() {
 		Utils.modal.reset('create');
+		$('#grade-record-table input[name^=grade-record-]').css({
+			'border' : '0',
+			'background-color' : '#fff'
+		});
 		Utils.modal.show('create');
 		$('#grade-record-table').parent().scrollTop(0);
 	});
@@ -113,23 +121,64 @@ define(function(require, exports, module) {
 
 		// 验证
 		// 实得分
-		var validateResult = true;
+		var validateResult1 = true;
+		var validateResult2 = true;
+		var validateResult3 = true;
+		var errorInputNames = [];
 		$.each($('#grade-record-table input[name^=grade-record-]'), function(key, value) {
-			if ($(value).val() === '') {
-				validateResult &= false;
+			var inputName = $(value).attr('name');
+			var inputValue = $(value).val();
+
+			// 恢复样式
+			$('input[name=' + inputName + ']').css({
+				'border' : '0',
+				'background-color' : '#fff'
+			});
+
+			// 不能为空
+			if (inputValue === '') {
+				validateResult1 &= false;
+				errorInputNames.push(inputName);
+			}
+
+			// 不能为负数
+			if (!(/^\d+(.\d+)?$/.test(inputValue))) {
+				validateResult2 &= false;
+				errorInputNames.push(inputName);
+			}
+
+			// 不能大于标准分
+			if (inputValue > $(value).data('max')) {
+				validateResult3 &= false;
+				errorInputNames.push(inputName);
 			}
 
 			object.gradeRecords.push({
-				row : $(value).attr('name').split('-')[2],
-				col : $(value).attr('name').split('-')[3],
-				content : $(value).val()
+				row : inputName.split('-')[2],
+				col : inputName.split('-')[3],
+				content : inputValue
 			});
 
-			object.score += parseFloat($(value).val());
+			object.score += parseFloat(inputValue);
 		});
 
-		if (!validateResult) {
+		// 高亮错误的input
+		$.each(errorInputNames, function(k, v) {
+			$('input[name=' + v + ']').css({
+				'border' : '1px solid #b94a48',
+				'background-color' : '#f2dede'
+			});
+		});
+		if (!validateResult1) {
 			Utils.modal.message('create', [ '请输入全部实得分' ]);
+			return;
+		}
+		if (!validateResult2) {
+			Utils.modal.message('create', [ '实得分必须为正数' ]);
+			return;
+		}
+		if (!validateResult3) {
+			Utils.modal.message('create', [ '实得分不能大于标准分' ]);
 			return;
 		}
 
@@ -202,12 +251,14 @@ define(function(require, exports, module) {
 		});
 	});
 
+	// 给查看表格添加行列id
 	$.each($('#grade-record-view-table tr:not(:first)'), function(key, value) {
 		var tds = $(value).find('td');
 		$.each(tds, function(k, v) {
 			if (tds.length >= 5) {
 				if (k + (8 - tds.length + 1) > 6) {
-					$(v).attr('id', 'grade-record-view-' + (key + 1) + '-' + (k + (8 - tds.length + 1)));
+					var rank = (key + 1) + '-' + (k + (8 - tds.length + 1));
+					$(v).attr('id', 'grade-record-view-' + rank);
 				}
 			}
 		});
