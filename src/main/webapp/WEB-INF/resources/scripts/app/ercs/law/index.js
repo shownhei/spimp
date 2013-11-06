4a3706868a4c3fb27e38fc8caf0c0c37cef851bd
zhaopeifu <344293750@qq.com>
2013-11-06 15:51:30 +0800

    应急法规的组织机构校验

--- src/main/webapp/WEB-INF/resources/scripts/app/ercs/law/index.js
+++ src/main/webapp/WEB-INF/resources/scripts/app/ercs/law/index.js
@@ -86,12 +86,15 @@ define(function(require, exports, module) {
 			Utils.modal.message('create', [ '请输入文件号' ]);
 			return;
 		}
-		// 验证
 		if (object.fileName === '') {
 			Utils.modal.message('create', [ '请输入文件名' ]);
 			return;
 		}
-		// 验证
+		
+		if (object.department === '') {
+			Utils.modal.message('create', [ '发布单位不能为空' ]);
+			return;
+		}
 		if (object.attachment === '') {
 			Utils.modal.message('create', [ '附件不能为空' ]);
 			return;
