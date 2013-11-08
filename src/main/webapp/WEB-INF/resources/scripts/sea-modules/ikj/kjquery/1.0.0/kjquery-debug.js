c56b79a36b7bcfcc7302b83b9fbf366bb9257949
shelltea <shelltea@gmail.com>
2013-11-08 16:14:01 +0800

    #129 增加富文本控件 http://xheditor.com/ #104 增加日期时间、时间控件 http://www.malot.fr/bootstrap-datetimepicker/index.php

--- src/main/webapp/WEB-INF/resources/scripts/sea-modules/ikj/kjquery/1.0.0/kjquery-debug.js
+++ src/main/webapp/WEB-INF/resources/scripts/sea-modules/ikj/kjquery/1.0.0/kjquery-debug.js
@@ -1,13 +1,15 @@
-define("ikj/kjquery/1.0.0/kjquery-debug", [ "$-debug", "ikj/jquery-ui/1.10.3/jquery-ui-debug", "ikj/rest/1.0.0/rest-debug", "ikj/ztree/3.5.14/ztree-debug", "ikj/gritter/1.0.0/gritter-debug", "ikj/bootstrap/2.3.2/bootstrap-debug", "ikj/datepicker/1.2.0/datepicker-debug", "ikj/daterangepicker/1.2.0/daterangepicker-debug", "ikj/serialize-object/1.0.0/serialize-object-debug", "ikj/autocomplete/1.2.3/autocomplete-debug" ], function(require, exports, module) {
+define("ikj/kjquery/1.0.0/kjquery-debug", [ "$-debug", "ikj/jquery-migrate/1.2.1/jquery-migrate-debug", "ikj/jquery-ui/1.10.3/jquery-ui-debug", "ikj/rest/1.0.0/rest-debug", "ikj/ztree/3.5.14/ztree-debug", "ikj/gritter/1.0.0/gritter-debug", "ikj/bootstrap/2.3.2/bootstrap-debug", "ikj/datepicker/1.2.0/datepicker-debug", "ikj/datetimepicker/2.0.0/datetimepicker-debug", "ikj/serialize-object/1.0.0/serialize-object-debug", "ikj/autocomplete/1.2.3/autocomplete-debug", "ikj/xheditor/1.2.1/xheditor-debug" ], function(require, exports, module) {
     var $ = require("$-debug");
+    require("ikj/jquery-migrate/1.2.1/jquery-migrate-debug");
     require("ikj/jquery-ui/1.10.3/jquery-ui-debug");
     require("ikj/rest/1.0.0/rest-debug");
     require("ikj/ztree/3.5.14/ztree-debug");
     require("ikj/gritter/1.0.0/gritter-debug");
     require("ikj/bootstrap/2.3.2/bootstrap-debug");
     require("ikj/datepicker/1.2.0/datepicker-debug");
-    require("ikj/daterangepicker/1.2.0/daterangepicker-debug");
+    require("ikj/datetimepicker/2.0.0/datetimepicker-debug");
     require("ikj/serialize-object/1.0.0/serialize-object-debug");
     require("ikj/autocomplete/1.2.3/autocomplete-debug");
+    require("ikj/xheditor/1.2.1/xheditor-debug");
     module.exports = $;
 });
