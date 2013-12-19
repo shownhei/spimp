2e1a469573962d1a7194aee70532881b1731d6de
shelltea <shelltea@gmail.com>
2013-12-19 15:46:12 +0800

    修正字典数据按列表返回时只返回第一页数据的问题

--- src/main/java/cn/ccrise/spimp/system/web/DictionaryController.java
+++ src/main/java/cn/ccrise/spimp/system/web/DictionaryController.java
@@ -3,10 +3,12 @@
  */
 package cn.ccrise.spimp.system.web;
 
-import java.util.ArrayList;
-
-import javax.validation.Valid;
-
+import cn.ccrise.ikjp.core.security.service.impl.LogEntityServiceImpl;
+import cn.ccrise.ikjp.core.util.Page;
+import cn.ccrise.ikjp.core.util.Response;
+import cn.ccrise.spimp.system.entity.Dictionary;
+import cn.ccrise.spimp.system.service.DictionaryService;
+import org.apache.commons.lang3.BooleanUtils;
 import org.apache.commons.lang3.StringUtils;
 import org.hibernate.criterion.MatchMode;
 import org.hibernate.criterion.Restrictions;
@@ -16,96 +18,82 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.MessageSource;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.ExceptionHandler;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.*;
 
-import cn.ccrise.ikjp.core.security.service.impl.LogEntityServiceImpl;
-import cn.ccrise.ikjp.core.util.Page;
-import cn.ccrise.ikjp.core.util.Response;
-import cn.ccrise.spimp.system.entity.Dictionary;
-import cn.ccrise.spimp.system.service.DictionaryService;
+import javax.validation.Valid;
+import java.util.ArrayList;
 
 /**
  * Dictionary Controller。
- * 
+ *
  * @author Xiong Shuhong(shelltea@gmail.com)
  */
 @Controller
 public class DictionaryController {
-	protected final Logger logger = LoggerFactory.getLogger(getClass());
+    protected final Logger logger = LoggerFactory.getLogger(getClass());
+    @Autowired
+    private DictionaryService dictionaryService;
 
-	@Autowired
-	private DictionaryService dictionaryService;
+    @RequestMapping(value = "/system/dictionaries/{id}", method = RequestMethod.DELETE)
+    @ResponseBody
+    public Response delete(@PathVariable long id) {
+        return new Response(dictionaryService.delete(id));
+    }
 
-	@Autowired
-	private LogEntityServiceImpl logEntityServiceImpl;
+    @RequestMapping(value = "/system/dictionaries/{id}", method = RequestMethod.GET)
+    @ResponseBody
+    public Response get(@PathVariable long id) {
+        return new Response(dictionaryService.get(id));
+    }
 
-	@Autowired
-	private MessageSource messageSource;
+    @ExceptionHandler
+    @ResponseBody
+    public Response handleException(Exception error) {
+        Response tResponse = new Response(false);
+        tResponse.setData(error.getMessage());
+        return tResponse;
+    }
 
-	@RequestMapping(value = "/system/dictionaries/{id}", method = RequestMethod.DELETE)
-	@ResponseBody
-	public Response delete(@PathVariable long id) {
-		return new Response(dictionaryService.delete(id));
-	}
+    @RequestMapping(value = "/system/dictionaries", method = RequestMethod.GET)
+    @ResponseBody
+    public Response page(Page<Dictionary> page, String typeCode, Boolean list, String itemName) {
+        ArrayList<SimpleExpression> param = new ArrayList<SimpleExpression>();
+        if (StringUtils.isNotBlank(typeCode)) {
+            param.add(Restrictions.eq("typeCode", typeCode));
+        }
+        if (StringUtils.isNotBlank(itemName)) {
+            param.add(Restrictions.like("itemName", itemName, MatchMode.ANYWHERE));
+        }
 
-	@RequestMapping(value = "/system/dictionaries/{id}", method = RequestMethod.GET)
-	@ResponseBody
-	public Response get(@PathVariable long id) {
-		return new Response(dictionaryService.get(id));
-	}
+        if (BooleanUtils.isTrue(list)) {
+            return new Response(dictionaryService.find(param.toArray(new SimpleExpression[0])));
+        }else{
+            page.setOrder("asc");
+            page.setOrderBy("sortIndex");
+            page = dictionaryService.getPage(page, param.toArray(new SimpleExpression[0]));
+            return new Response(page);
+        }
+    }
 
-	@ExceptionHandler
-	@ResponseBody
-	public Response handleException(Exception error) {
-		Response tResponse = new Response(false);
-		tResponse.setData(error.getMessage());
-		return tResponse;
-	}
+    @RequestMapping(value = "/system/dictionaries/all", method = RequestMethod.GET)
+    @ResponseBody
+    public Response pageAll(Page<Dictionary> page) {
+        ArrayList<SimpleExpression> param = new ArrayList<SimpleExpression>();
+        page.setOrder("asc");
+        page.setOrderBy("sortIndex");
+        page = dictionaryService.getPage(page, param.toArray(new SimpleExpression[0]));
+        return new Response(page.getResult());
+    }
 
-	@RequestMapping(value = "/system/dictionaries", method = RequestMethod.GET)
-	@ResponseBody
-	public Response page(Page<Dictionary> page, String typeCode, Boolean list, String itemName) {
-		ArrayList<SimpleExpression> param = new ArrayList<SimpleExpression>();
-		if (StringUtils.isNotBlank(typeCode)) {
-			param.add(Restrictions.eq("typeCode", typeCode));
-		}
-		if (StringUtils.isNotBlank(itemName)) {
-			param.add(Restrictions.like("itemName", itemName, MatchMode.ANYWHERE));
-		}
-		page.setOrder("asc");
-		page.setOrderBy("sortIndex");
-		page = dictionaryService.getPage(page, param.toArray(new SimpleExpression[0]));
+    @RequestMapping(value = "/system/dictionaries", method = RequestMethod.POST)
+    @ResponseBody
+    public Response save(@RequestBody Dictionary dictionary) {
+        return new Response(dictionaryService.save(dictionary));
+    }
 
-		if (list != null && list.booleanValue()) {
-			return new Response(page.getResult());
-		}
-		return new Response(page);
-	}
-
-	@RequestMapping(value = "/system/dictionaries/all", method = RequestMethod.GET)
-	@ResponseBody
-	public Response pageAll(Page<Dictionary> page) {
-		ArrayList<SimpleExpression> param = new ArrayList<SimpleExpression>();
-		page.setOrder("asc");
-		page.setOrderBy("sortIndex");
-		page = dictionaryService.getPage(page, param.toArray(new SimpleExpression[0]));
-		return new Response(page.getResult());
-	}
-
-	@RequestMapping(value = "/system/dictionaries", method = RequestMethod.POST)
-	@ResponseBody
-	public Response save(@RequestBody Dictionary dictionary) {
-		return new Response(dictionaryService.save(dictionary));
-	}
-
-	@RequestMapping(value = "/system/dictionaries/{id}", method = RequestMethod.PUT)
-	@ResponseBody
-	public Response update(@Valid @RequestBody Dictionary dictionary, @PathVariable long id) {
-		return new Response(dictionaryService.update(dictionary));
-	}
+    @RequestMapping(value = "/system/dictionaries/{id}", method = RequestMethod.PUT)
+    @ResponseBody
+    public Response update(@Valid @RequestBody Dictionary dictionary, @PathVariable long id) {
+        return new Response(dictionaryService.update(dictionary));
+    }
 }
