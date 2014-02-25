d008b9e2e7c52ad92965ade83b00eacca62fb799
zhaopeifu <344293750@qq.com>
2014-02-25 15:16:14 +0800

    配件管理--配件统计 合并同月入库记录

--- src/main/java/cn/ccrise/spimp/electr/web/BlottersController.java
+++ src/main/java/cn/ccrise/spimp/electr/web/BlottersController.java
@@ -10,6 +10,7 @@ import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 import javax.validation.Valid;
 
+import org.apache.commons.lang3.StringUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -94,6 +95,9 @@ public class BlottersController {
 				.getString(PropertiesUtils.SESSION_KEY_PROPERTY));
 		blotters.setRecordGroup(loginAccount.getGroupEntity());
 		blotters.setRecordTime(new Timestamp(System.currentTimeMillis()));
+		if(StringUtils.isBlank(blotters.getOperator())){
+			blotters.setOperator(loginAccount.getRealName());
+		}
 		if (blotters.getOpertionType() == Blotters.OPERTION_TYPE_PUTIN) {
 			return new Response(blottersService.putIn(blotters, httpSession));
 		} else {
