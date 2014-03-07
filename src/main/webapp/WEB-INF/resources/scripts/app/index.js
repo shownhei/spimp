b5beb9fabcce3912a50df977fadcf7da828ab2d2
shelltea <shelltea@gmail.com>
2014-03-07 09:50:26 +0800

    修正首页图表js代码的各种小问题

--- src/main/webapp/WEB-INF/resources/scripts/app/index.js
+++ src/main/webapp/WEB-INF/resources/scripts/app/index.js
@@ -48,59 +48,53 @@ define(function(require, exports, module) {
 		});
 	});
 
-	
-	// chart2--年度油耗
-	var showChart2 = function() {
-		var chart2 = echarts.init(document.getElementById('chart2'));
-		$('#chart2-title').html(new Date().format('yyyy年') + '年度运行统计');
+	// 图表2
+	var chart2 = echarts.init(document.getElementById('chart2'));
+	$('#chart2-title').html(new Date().format('yyyy年') + '运行统计');
+	$.get(contextPath + '/electr/car/annual-oil/resultchart?year=' + new Date().format('yyyy'), function(data) {
 		var carTypes = [];
-		var cars = [];
 		var runs = [];
-		var run = 0;
 		var kilomiters = [];
-		var kilomiter = 0;
 		var oils = [];
-		var oil = 0;
-		$.get(contextPath + '/electr/car/annual-oil/resultchart?year=' + new Date().format('yyyy'), function(data) {
-			var result = data.data.category;
-			var container = {};
-			$.each(result, function(k, v) {
-				if (v) {
-					var len = v.length;
-					var currentType = null;
-					for (var i = 0; i < len; i++) {
-						if (carTypes.indexOf(v[i].carCategory) === -1) {
-							currentType = {
-								run : 0,
-								kilomiter : 0,
-								oil : 0
-							};
-							container[v[i].carCategory] = currentType;
-							carTypes.push(v[i].carCategory);
-						} else {
-							currentType = container[v[i].carCategory];
-						}
-						currentType.run += v[i].trainNumber;
-						currentType.kilomiter += v[i].distance;
-						currentType.oil += v[i].refuelNumber;
+		var container = {};
+
+		$.each(data.data.category, function(k, v) {
+			if (v) {
+				var len = v.length;
+				var currentType = null;
+				for (var i = 0; i < len; i++) {
+					if (carTypes.indexOf(v[i].carCategory) === -1) {
+						currentType = {
+							run : 0,
+							kilomiter : 0,
+							oil : 0
+						};
+						container[v[i].carCategory] = currentType;
+						carTypes.push(v[i].carCategory);
+					} else {
+						currentType = container[v[i].carCategory];
 					}
+					currentType.run += v[i].trainNumber;
+					currentType.kilomiter += v[i].distance;
+					currentType.oil += v[i].refuelNumber;
 				}
-			});//
-			$.each(carTypes, function(k, v) {
-				runs.push(container[v].run);
-				kilomiters.push(container[v].kilomiter);
-				oils.push(container[v].oil);
-			});
-			chart2.setOption(yearOidOption);
+			}
 		});
-		var yearOidOption = {
+
+		$.each(carTypes, function(k, v) {
+			runs.push(container[v].run);
+			kilomiters.push(container[v].kilomiter);
+			oils.push(container[v].oil);
+		});
+
+		chart2.setOption({
 			color : colors,
 			tooltip : {
 				trigger : 'axis'
 			},
 			legend : {
 				orient : 'horizontal',
-				data : [ '运行次数', '行驶公里数', '加油数'],
+				data : [ '运行次数', '行驶公里数', '加油数' ],
 				x : 'center',
 				y : 'top'
 			},
@@ -130,63 +124,59 @@ define(function(require, exports, module) {
 				name : '加油数',
 				type : 'bar',
 				data : oils
-			}]
-		};
-	};
-	showChart2();
-	var showChart3 = function() {
-		var chart3 = echarts.init(document.getElementById('chart3'));
-		$('#chart3-title').html(new Date().format('yyyy年') + '运行统计');
-		var months=["1月","2月","3月","4月","5月","6月","7月","8月","9月","10月","11月","12月"];
-		var datas=[];
-		$.get(contextPath + '/electr/car/annual-kilometer/chart?year=' + new Date().format('yyyy'), function(data) {
-			var result = data.data.result;
-			var kilos=0;
-			var kiloArray=null;
-			$.each(result, function(k, v) {
-				kilos=0;
-				var _tempLen=v.length;
-				$.each(v, function(key, val) {
-					if(key<_tempLen-1){
-						kilos+=val;
-					}
-				});
-				datas.push(kilos);
-			});
-			chart3.setOption(yearKilosOption);
+			} ]
 		});
-			var yearKilosOption = {
-					color : colors,
-					tooltip : {
-						trigger : 'item',
-						formatter : '{a} <br/>{b} : {c}公里 '
-					},
-					legend : {
-						orient : 'horizontal',
-						data : [ '运行公里数'],
-						x : 'center',
-						y : 'top'
-					},
-					calculable : false,
-					xAxis : [ {
-						type : 'category',
-						data : months,
-						axisLabel : {
-							rotate : '30'
-						}
-					} ],
-					yAxis : [ {
-						type : 'value',
-						splitArea : {
-							show : true
-						}
-					} ],
-					series : [ {
-						name : '运行公里数',
-						type : 'bar',
-						data : datas
-					} ]
-				};
-	};
-	showChart3();
+	});
+
+	// 图表3
+	var chart3 = echarts.init(document.getElementById('chart3'));
+	$('#chart3-title').html(new Date().format('yyyy年') + '运行公里数统计');
+	$.get(contextPath + '/electr/car/annual-kilometer/chart?year=' + new Date().format('yyyy'), function(data) {
+		var kilos = 0;
+		var datas = [];
+
+		$.each(data.data.result, function(k, v) {
+			kilos = 0;
+			var _tempLen = v.length;
+			$.each(v, function(key, val) {
+				if (key < _tempLen - 1) {
+					kilos += val;
+				}
+			});
+			datas.push(kilos);
+		});
+
+		chart3.setOption({
+			color : colors,
+			tooltip : {
+				trigger : 'item',
+				formatter : '{a} <br/>{b} : {c}公里 '
+			},
+			legend : {
+				orient : 'horizontal',
+				data : [ '运行公里数' ],
+				x : 'center',
+				y : 'top'
+			},
+			calculable : false,
+			xAxis : [ {
+				type : 'category',
+				data : [ "1月", "2月", "3月", "4月", "5月", "6月", "7月", "8月", "9月", "10月", "11月", "12月" ],
+				axisLabel : {
+					rotate : '30'
+				}
+			} ],
+			yAxis : [ {
+				type : 'value',
+				splitArea : {
+					show : true
+				}
+			} ],
+			series : [ {
+				name : '运行公里数',
+				type : 'bar',
+				data : datas
+			} ]
+		});
+	});
 });
