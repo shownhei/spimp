55e14a454b1c8e2d96c845e21947154ef3026d98
zhaopeifu <344293750@qq.com>
2014-04-17 14:13:01 +0800

    实时最大瓦斯浓度数值及其传感器位置，实时最大一氧化碳浓度数值及其传感器位置、当天最大瓦斯浓度数值及其传感器位置、当天最大一氧化碳浓度数值及其传感器位置

--- src/main/webapp/WEB-INF/resources/scripts/app/3d/index.js
+++ src/main/webapp/WEB-INF/resources/scripts/app/3d/index.js
@@ -344,6 +344,29 @@ define(function(require, exports, module) {
 			var footer=']}';
 			WebMineSystem._Rydw_SetWellLeader(header+buff.join(',')+footer);
 		});
+		//3 实时最大瓦斯浓度数值及其传感器位置
+		$.get(contextPath + '/monitor/monitor-nodes/max?sensorTypeId=1', function(data) {
+			//{"success":true,"data":[[0.3,"E302工作面上隅角瓦斯"]],"errors":null}
+			//{“CH4”:0.5 }
+			WebMineSystem.SF_SetRealMaxCH4('{"CH4":'+data.data[0][0]+'}');
+		});
+		//4 实时最大一氧化碳浓度数值及其传感器位置
+        $.get(contextPath + '/monitor/monitor-nodes/max?sensorTypeId=4', function(data) {
+        	//{"success":true,"data":[[1.5,"E302进风顺槽3部机头一氧化碳"],[1.5,"E302进风顺槽1部机头一氧化碳"]],"errors":null}
+			//{“CO”:0.5 }
+        	WebMineSystem.SF_SetRealMaxCO('{"CO":'+data.data[0][0]+'}');
+		});
+        //5 当天最大瓦斯浓度数值及其传感器位置
+        var currentDate = new Date();
+        var dateStr=currentDate.getFullYear()+'-'+(currentDate.getMonth()+1)+'-'+currentDate.getDate();
+        $.get(contextPath + '/monitor/monitor-real-datas/max?sensorTypeId=1&date='+dateStr, function(data) {
+			//{"success":true,"data":[[0.22,"9#煤水泵房工作面回风甲烷"]],"errors":null}
+		});
+        //6 当天最大一氧化碳浓度数值及其传感器位置
+        $.get(contextPath + '/monitor/monitor-real-datas/max?sensorTypeId=4&date='+dateStr, function(data) {
+        	//{"success":true,"data":[[2.75,"采区回风巷一氧化碳"]],"errors":null}
+			
+		});        
 	},1000);
 	$(document).click(function(event) {
 		var el = $(event.target);
