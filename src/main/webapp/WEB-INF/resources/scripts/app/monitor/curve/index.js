194ef9445e3f5117906016f502461e2c6fa5490e
xuzhiqi <jackie.214@163.com>
2014-05-21 10:14:09 +0800

    曲线完善

--- src/main/webapp/WEB-INF/resources/scripts/app/monitor/curve/index.js
+++ src/main/webapp/WEB-INF/resources/scripts/app/monitor/curve/index.js
@@ -33,16 +33,16 @@ define(function (require, exports, module) {
 		loadChart();
 	});
 
+
+	
 	// 显示图形
 	var chart;
 	function loadChart() {
 		if (chart !== undefined) {
 			chart.dispose();
 		}
-		var param = Utils.form.serialize('query');
-
-		$.get('history-curve?nodeIds=' + param.nodePlace + '&date='
-			 + param.date, function (data) {
+		var param = Utils.form.serialize('query');	
+		$.get('history-curve?nodeIds=' + param.nodePlace + '&date=' + param.date, function (data) {
 			// 如果数据为空，给出提示，并返回
 			var result = data.data;
 			if (result.length < 1) {
@@ -60,32 +60,16 @@ define(function (require, exports, module) {
 				var avgArray = new Array(dataLength);
 				var maxArray = new Array(dataLength);
 				var minArray = new Array(dataLength);
-
 				
 				for (var i = 0; i < dataLength; i++) {
-					dateTimeArray[i] = convertDate(currentNodeData[i].dataTime);
+					dateTimeArray[i] = getTimes(currentNodeData[i].dataTime);
 					avgArray[i] = currentNodeData[i].avgData;
 					maxArray[i] = currentNodeData[i].maxData;
 					minArray[i] = currentNodeData[i].minData;
-				}
-				
-//				var scale = new Array(6);
-//				scale[0]= Math.max.apply(null, avgArray);
-//				scale[1]= Math.min.apply(null, avgArray);
-//				scale[2]= Math.max.apply(null, maxArray);
-//				scale[3]= Math.min.apply(null, maxArray);
-//				scale[4]= Math.max.apply(null, minArray);
-//				scale[5]= Math.min.apply(null, minArray);
-//				
-//				var a = Math.max.apply(null, scale);
-//				var b = Math.min.apply(null, scale);
-//				console.log(a);
-//				console.log(b);
-	
+				}	
 			}
 
-			chart = echarts.init(document
-					.getElementById('historyChart'));
+			chart = echarts.init(document.getElementById('historyChart'));
 
 			option = {
 				title : {
@@ -103,23 +87,27 @@ define(function (require, exports, module) {
 						mark : {
 							show : true
 						},
-						dataView : {
-							show : true,
-							readOnly : false
-						},
-						magicType : {
-							show : true,
-							type : ['line', 'bar']
-						},
 						restore : {
-							show : true
+							show: true
 						},
 						saveAsImage : {
 							show : true
 						}
 					}
 				},
-				calculable : true,
+				calculable : false,
+				dataZoom : {
+					//控制数据横轴缩放
+			        show : true,
+			        realtime : true,
+			        height: 30,
+			        backgroundColor: 'rgba(221,160,221,0.5)',
+			        dataBackgroundColor: 'rgba(138,43,226,0.5)',
+			        fillerColor: 'rgba(38,143,26,0.6)',
+			        handleColor: 'rgba(128,43,16,0.8)',
+			        start : 20,
+			        end : 50
+			    },
 				xAxis : [{
 						type : 'category',
 						boundaryGap : false,
@@ -127,16 +115,16 @@ define(function (require, exports, module) {
 						axisLabel:{
 							show: true
 							//横坐标标记倾斜45度
-//							rotate:45
+							//rotate:45
 						}
 					}
 				],
 				yAxis : [{
 						type : 'value',
 						//控制显示效果，设置曲线显示的上下限
-//						min: b,
-//						max: a,
-//						scale: true,
+						//min: b,
+						//max: a,
+						scale: true,
 						splitArea : {
 							show : true
 						}
@@ -145,26 +133,38 @@ define(function (require, exports, module) {
 				series : [{
 						name : '最大值',
 						type : 'line',
+						symbol: 'none',
 						data : maxArray
 					}, {
 						name : '最小值',
 						type : 'line',
+						symbol: 'none',
 						data : minArray
 					}, {
 						name : '平均值',
 						type : 'line',
+						symbol: 'none',
 						data : avgArray
 					}
 				]
 			};
+			
 			chart.setOption(option);
 
 		});
 	}
-	function convertDate(dateStr) {
-		var hours= new Date(dateStr).getHours();
-		var minutes= new Date(dateStr).getMinutes();
-		return hours+"点"+minutes+"分";
+	
+	//String类型日期时间转换成格林威治类型时间
+	var convertToDate=function(str){
+		var timetmp = str.replace(/-/g,"/");
+		var date = new Date(timetmp);
+		return date;
+	};
+	//获取时间作为x轴坐标
+	var getTimes = function(dateStr) {
+		var hours = (convertToDate(dateStr)).getHours();
+		var minutes = (convertToDate(dateStr)).getMinutes();
+		return hours+":"+minutes;
 	}
 
 });
