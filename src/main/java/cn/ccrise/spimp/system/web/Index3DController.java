1ba64f9fa72e6652b0a625fa594176dc07551fad
zhaopeifu <344293750@qq.com>
2014-05-27 16:46:07 +0800

    限制3d界面的非登录使用

--- src/main/java/cn/ccrise/spimp/system/web/Index3DController.java
+++ src/main/java/cn/ccrise/spimp/system/web/Index3DController.java
@@ -7,6 +7,8 @@ import java.io.UnsupportedEncodingException;
 import java.util.HashMap;
 import java.util.LinkedList;
 
+import javax.servlet.http.HttpSession;
+
 import org.apache.commons.lang.StringUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -15,6 +17,10 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
+
+import cn.ccrise.ikjp.core.security.service.util.WebUtils;
+import cn.ccrise.ikjp.core.util.PropertiesUtils;
+import cn.ccrise.spimp.system.entity.Account;
 
 /**
  * @author Xiong Shuhong(shelltea@gmail.com)
@@ -30,13 +36,18 @@ public class Index3DController {
 	}
 
 	@RequestMapping(value = "/3d.html", method = RequestMethod.GET)
-	public String dd() {
+	public String dd(HttpSession httpSession) {
 		return "3d/3d";
 	}
 
 	// 三维综合管理
 	@RequestMapping(value = "/3d", method = RequestMethod.GET)
-	public String ddd() {
+	public String ddd(HttpSession httpSession) {
+		Account account = ((Account) WebUtils.getAccount(httpSession));
+		if(account==null){
+			return "redirect:" + PropertiesUtils.getString(PropertiesUtils.LOGIN_PATH_PROPERTY);
+		}
+		logger.debug("{}",account);
 		return "3d/index";
 	}
 	//人机环对话框界面
